22ffd1e8de7caf8c6295d7dad4dd61973d333aa0
YuSaki丶Kanade <nep_timeline@outlook.com>
2024-07-15 14:06:59 +0800

    Update MultiThread and code opt

--- nep/timeline/EventSystem/EventManager.java
+++ nep/timeline/EventSystem/EventManager.java
@@ -1,6 +1,5 @@
 package nep.timeline.EventSystem;
 
-import nep.timeline.EventSystem.threads.MethodInvoke;
 import nep.timeline.EventSystem.type.EventPriority;
 
 import java.lang.invoke.MethodHandle;
@@ -9,12 +8,17 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 public class EventManager
 {
     public static final MethodHandles.Lookup lookup = MethodHandles.lookup();
     private static final CopyOnWriteArrayList<Object> listeners = new CopyOnWriteArrayList<>();
+    private static final ExecutorService executorService = Executors.newCachedThreadPool();
 
     public static void reset()
     {
@@ -31,104 +35,116 @@ public class EventManager
     public static void removeListener(Object... projects)
     {
         if (!listeners.isEmpty())
-            Arrays.asList(projects).forEach(listeners::remove);
+            listeners.removeAll(Arrays.asList(projects));
     }
 
     public static void callEvent(EventCore event, Object listener, Method method)
     {
-        try
-        {
-            MethodHandle handle = EventManager.lookup.unreflect(method);
-            EventListener annotation = method.getDeclaredAnnotation(EventListener.class);
-            if (annotation == null)
-                return;
-
-            EventList events = annotation.event();
-
-            if (events != EventList.NONE && (events == event.getEvent() || events == EventList.ALL))
+        synchronized (EventManager.class) {
+            try
             {
-                if (method.getParameterCount() == 1)
+                MethodHandle handle = lookup.unreflect(method);
+                EventListener annotation = method.getDeclaredAnnotation(EventListener.class);
+                if (annotation == null)
+                    return;
+
+                EventList events = annotation.event();
+
+                if (events != EventList.NONE && (events == event.getEvent() || events == EventList.ALL))
+                {
+                    if (method.getParameterCount() == 1)
+                    {
+                        Class<?> methodObject = method.getParameterTypes()[0];
+                        if (methodObject != event.getClass())
+                            throw new EventException("[EventSystem] The event does not match! method name:" + method.getName());
+                        handle.invoke(listener, event);
+                    }
+                    else
+                    {
+                        handle.invoke(listener);
+                    }
+                }
+                else if (events == EventList.NONE && method.getParameterCount() == 1)
                 {
                     Class<?> methodObject = method.getParameterTypes()[0];
-                    if (methodObject != event.getClass())
-                        throw new EventException("[EventSystem] The event does not match! method name:" + method.getName());
-                    handle.invoke(listener, event);
+
+                    if (methodObject != null && methodObject == event.getClass())
+                        handle.invoke(listener, event);
                 }
-                else
+                else if (events == event.getEvent())
                 {
-                    handle.invoke(listener);
+                    throw new EventException("Incorrect usage! method name:" + method.getName());
                 }
             }
-            else if (events == EventList.NONE && method.getParameterCount() == 1)
+            catch (InvocationTargetException | IllegalAccessException ignored)
             {
-                Class<?> methodObject = method.getParameterTypes()[0];
 
-                if (methodObject != null && methodObject == event.getClass())
-                    handle.invoke(listener, event);
             }
-            else if (events == event.getEvent())
+            catch (Throwable throwable)
             {
-                throw new EventException("Incorrect usage! method name:" + method.getName());
+                throwable.printStackTrace();
             }
-        }
-        catch (InvocationTargetException | IllegalAccessException ignored)
-        {
-
-        }
-        catch (Throwable throwable)
-        {
-            throwable.printStackTrace();
         }
     }
 
     public static EventCore call(EventCore event)
     {
-        return call(event, false);
+        return call(event, true);
     }
 
-    public static EventCore call(EventCore event, boolean multi_thread)
+    public static EventCore call(EventCore event, boolean multiThread)
+    {
+        List<CompletableFuture<Void>> futures = listeners.stream().flatMap(listener -> {
+            List<Method> methods = Arrays.stream(listener.getClass().getDeclaredMethods())
+                    .filter(method -> method.isAnnotationPresent(EventListener.class))
+                    .sorted((method1, method2) -> {
+                        EventPriority priority1 = method1.getDeclaredAnnotation(EventListener.class).priority();
+                        EventPriority priority2 = method2.getDeclaredAnnotation(EventListener.class).priority();
+                        return Integer.compare(priority1.getLevel(), priority2.getLevel());
+                    }).collect(Collectors.toList());
+
+            return methods.stream().map(method -> {
+                if (!method.canAccess(listener))
+                    method.setAccessible(true);
+
+                if (method.getParameterCount() > 1)
+                    throw new EventException("Too many method types! method name:" + method.getName());
+
+                Runnable task = () -> callEvent(event, listener, method);
+                return multiThread ? CompletableFuture.runAsync(task, executorService) : CompletableFuture.runAsync(task);
+            });
+        }).collect(Collectors.toList());
+
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
+
+        return event;
+    }
+
+    /*public static EventCore call(EventCore event, boolean multiThread)
     {
         listeners.forEach(listener -> {
-            List<Method> methods = Arrays.asList(listener.getClass().getDeclaredMethods());
-            methods.sort((method1, method2) -> {
-                if (!method1.isAccessible())
-                    method1.setAccessible(true);
-
-                if (!method2.isAccessible())
-                    method2.setAccessible(true);
-
-                EventPriority priority1 = null;
-                EventPriority priority2 = null;
-
-                if (method1.isAnnotationPresent(EventListener.class))
-                    priority1 = method1.getDeclaredAnnotation(EventListener.class).priority();
-
-                if (method2.isAnnotationPresent(EventListener.class))
-                    priority2 = method2.getDeclaredAnnotation(EventListener.class).priority();
-
-                if (priority1 != null && priority2 != null && priority1 != priority2)
-                    return priority1.getLevel() - priority2.getLevel();
-                else
-                    return 0;
-            });
+            List<Method> methods = Arrays.stream(listener.getClass().getDeclaredMethods())
+                    .filter(method -> method.isAnnotationPresent(EventListener.class))
+                    .sorted((method1, method2) -> {
+                        EventPriority priority1 = method1.getDeclaredAnnotation(EventListener.class).priority();
+                        EventPriority priority2 = method2.getDeclaredAnnotation(EventListener.class).priority();
+                        return Integer.compare(priority1.getLevel(), priority2.getLevel());
+                    }).collect(Collectors.toList());
 
             methods.forEach(method -> {
                 if (!method.isAccessible())
                     method.setAccessible(true);
 
-                if (method.isAnnotationPresent(EventListener.class))
-                {
-                    if (method.getParameterCount() > 1)
-                        throw new EventException("Too many method types! method name:" + method.getName());
+                if (method.getParameterCount() > 1)
+                    throw new EventException("Too many method types! method name:" + method.getName());
 
-                    if (multi_thread)
-                        new MethodInvoke("Multi Thread Invoke " + method.getName(), method, listener, event).start();
-                    else
-                        callEvent(event, listener, method);
-                }
+                if (multiThread)
+                    executorService.execute(() -> callEvent(event, listener, method));
+                else
+                    callEvent(event, listener, method);
             });
         });
 
         return event;
-    }
+    }*/
 }
